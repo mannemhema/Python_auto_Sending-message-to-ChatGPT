ee01d904781754615e13ca1af8ec5668ffe6bd84
caogiangnam <caogiangnam523@gmail.com>
2023-08-09 02:28:26 +0900

    single login, multi chat Demo

--- autoSendingchat.py
+++ autoSendingchat.py
@@ -7,9 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
-print("Question:")
-
-input_String = input()
+# Get mail and password from .txt outside
 credential_Array = []
 
 with open('credential/loginForGPT.txt') as f:
@@ -21,7 +19,7 @@ with open('credential/loginForGPT.txt') as f:
 
 mail = credential_Array[0]
 password = credential_Array[1]
-# Replace with the path to your ChromeDriver
+
 # Start a webdriver instance and open ChatGPT
 driver = webdriver.Chrome()
 driver.get('https://chat.openai.com')
@@ -33,30 +31,44 @@ try:
     login_button = driver.find_element(By.XPATH, '//div[text()="Log in"]').click()
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, "username")))
     email = driver.find_element(By.ID, "username").send_keys(mail)
+
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//button[text()="Continue"]')))
     continue_button = driver.find_element(By.XPATH, '//button[text()="Continue"]').click()
+
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, "password")))
     password = driver.find_element(By.ID, "password").send_keys(password)
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.CLASS_NAME, "_button-login-password")))
     continue_button_next = driver.find_element(By.CLASS_NAME, "_button-login-password").click()
+
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//div[text()="Next"]')))
     next_button = driver.find_element(By.XPATH, '//div[text()="Next"]').click()
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//div[text()="Next"]')))
     next_button = driver.find_element(By.XPATH, '//div[text()="Next"]').click()
     WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//div[text()="Done"]')))
     next_button = driver.find_element(By.XPATH, '//div[text()="Done"]').click()
-    prompt_textarea = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, "prompt-textarea")))
-    input_textarea = driver.find_element(By.ID, "prompt-textarea").send_keys(input_String)
-    prompt_textarea.send_keys(Keys.RETURN)
-    time.sleep(5)
-    outer_response = driver.find_element(By.CLASS_NAME, "markdown")
-    # Get children of outer_response
-    children = outer_response.find_elements(By.XPATH, '*')
-    # Itertae over the children
-    for child in children:
+
+    print("Question:")
+    input_String = input()
+    while input_String != "exit":
+        prompt_textarea = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.ID, "prompt-textarea")))
+        input_textarea = driver.find_element(By.ID, "prompt-textarea").send_keys(input_String)
+        prompt_textarea.send_keys(Keys.RETURN)
+
+        time.sleep(5)
+        outer_response = driver.find_element(By.CLASS_NAME, "markdown")
+
+        # Get children of outer_response
+        children = outer_response.find_elements(By.XPATH, '*')
+        # Itertae over the children
+        for child in children:
+            print("\n")
+            print("Answer:")
+            print(child.get_attribute('innerHTML'))
+        input_String = ""
+        print("-----------------------------------------------------------------------------------------------------------------------")
         print("\n")
-        print("Answer:")
-        print(child.get_attribute('innerHTML'))
+        print("Click 'exit' to close chatting!")
+        input_String = input()
 except TimeoutError:
     print("Network error")
 driver.quit()
